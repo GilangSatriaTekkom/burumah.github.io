feefd9dd15d91ac1b45c5e9472b61001970ef799
unknown <tekkomgilang26@gmail.com>
2024-08-29 14:28:58 +0700

    Progress finish (header 100%, Slider 100%)

--- script/custom.js
+++ script/custom.js
@@ -1,1 +1,17 @@
 console.log('testing js')
+
+
+// Add active class to the current button (highlight it)
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('#navActive li');
+    
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            // Remove 'active' class from all items
+            items.forEach(i => i.classList.remove('active'));
+            
+            // Add 'active' class to the clicked item
+            this.classList.add('active');
+        });
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,41 @@ export default {
         xl: '3rem',    // Padding for extra large screens
       },
     },
+
+    fontFamily: {
+      'satoshi': ['Satoshi', 'sans-serif'],
+    },
+
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '3.815rem',
+      '7xl': '4.6rem',
+      '8xl': '6rem',
+      '9xl': '8rem',  
+    },
+
+    keyframes: {
+      marquee: {
+        '0%': { transform: 'translateX(0%)' },
+        '100%': { transform: 'translateX(-100%)' },
+      },
+      marquee2: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0%)' },
+      },
+    },
+
+    animation: {
+      marquee: 'marquee 25s linear infinite',
+      marquee2: 'marquee2 25s linear infinite',
+    },
   },
   plugins: [],
 }
